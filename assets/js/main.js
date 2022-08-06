d4c66c7fb82f6d2092577552beae217d228e9870
mandeepb29 <mandeepb29@gmail.com>
2022-08-07 02:01:01 +0530

    homepage updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 feather.replace();
 
   function initializeOwlCarousel(){
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel-topright").owlCarousel({
         loop:true,
         margin:10,
         items:1,
@@ -13,7 +13,36 @@ feather.replace();
           `<button type="button" id="lg-prev-1" aria-label="Previous slide" class="btn-icon btn-icon-outline" style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/left-white.svg" /></button>`,
           `<button type="button" id="lg-next-1" aria-label="Next slide" class="btn-icon btn-icon-outline"  style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/right-white.svg" /> </button>`],
       });
-    console.log("initialize owlcarousel")
+
+
+      $(".owl-carousel-industry").owlCarousel({
+        loop:false,
+        margin:0,
+        items:4,
+        nav:true,
+        dots:false,
+        autoplay:false,
+        navElement:'div',
+        navText: [
+          `<button type="button" id="lg-prev-1" aria-label="Previous slide" class="btn-icon btn-icon-outline" style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/left-white.svg" /></button>`,
+          `<button type="button" id="lg-next-1" aria-label="Next slide" class="btn-icon btn-icon-outline"  style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/right-white.svg" /> </button>`],
+      });
+
+      $(".owl-carousel-testimonial").owlCarousel({
+        loop:false,
+        items:3,
+        nav:true,
+        autoWidth:true,
+        margin:20,
+        dots:false,
+        autoplay:false,
+        navElement:'div',
+        navText: [
+          `<button type="button" id="lg-prev-1" aria-label="Previous slide" class="btn-icon btn-icon-outline" style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/left-white.svg" /></button>`,
+          `<button type="button" id="lg-next-1" aria-label="Next slide" class="btn-icon btn-icon-outline"  style="opacity:1;right:0;top:0;"> <img src="../assets/img/icons/right-white.svg" /> </button>`],
+      });
+
+    //console.log("initialize owlcarousel")
   }
 
   function initializeFeatherIcons(){
