9886367765e99cfe3e8b64b3604e96c699c4e684
mandeepb29 <mandeepb29@gmail.com>
2022-08-30 00:50:01 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,15 @@ feather.replace();
         navText: [
           `<button type="button" id="lg-prev-1" aria-label="Previous slide" class="btn-icon btn-icon-outline" style="opacity:1;right:0;top:0;"> <img src="./assets/img/icons/left-white.svg" /></button>`,
           `<button type="button" id="lg-next-1" aria-label="Next slide" class="btn-icon btn-icon-outline"  style="opacity:1;right:0;top:0;"> <img src="./assets/img/icons/right-white.svg" /> </button>`],
+          responsive:{
+            0:{
+              nav:false
+            },
+            600:{
+             
+              nav:true
+            },
+        }
       });
 
       $(".owl-carousel-videos").owlCarousel({
